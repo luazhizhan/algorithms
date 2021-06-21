edfd869005d9b371cdaaedf4ba1b786f0e0e6d2b
Lua Zhi Zhan <zhizhanlua@gmail.com>
2021-06-21 09:15:44 +0800

    Add hash table seperate chaining

---
+++ __tests__/hashTableSeperateChaining.spec.ts
@@ -0,0 +1,188 @@
+import { HashTableSeparateChaining } from '../src/hashTableSeperateChaining'
+
+type HashMap = { [key in string]: unknown }
+const LOOPS = 50
+const MAX_SIZE = randInt(1, 30)
+const MAX_RAND_NUM = randInt(1, 10)
+
+function randInt(min: number, max: number): number {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function genRandList(sz: number): number[] {
+  const lst = []
+  for (let i = 0; i < sz; i++) {
+    lst.push(randInt(-MAX_RAND_NUM, MAX_RAND_NUM))
+  }
+  shuffle(lst)
+  return lst
+}
+
+function shuffle(array: number[]): number[] {
+  return array.sort(() => 0.5 - Math.random())
+}
+
+describe('HashTableSeparateChaining', () => {
+  it('Illegal creation', () => {
+    expect(() => new HashTableSeparateChaining(-3, 0.5)).toThrowError()
+    expect(
+      () => new HashTableSeparateChaining(5, Number.POSITIVE_INFINITY)
+    ).toThrowError()
+    expect(() => new HashTableSeparateChaining(6, -0.5)).toThrowError()
+  })
+
+  it('Update value', () => {
+    const map = new HashTableSeparateChaining<number, number>(6, 0.9)
+    map.add(1, 1)
+    expect(map.get(1)).toBe(1)
+
+    map.add(1, 5)
+    expect(map.get(1)).toBe(5)
+
+    map.add(1, -7)
+    expect(map.get(1)).toBe(-7)
+  })
+
+  it('Remove', () => {
+    const map = new HashTableSeparateChaining<number, number>()
+
+    // add 3 elements
+    map.put(11, 0)
+    map.put(12, 0)
+    map.put(13, 0)
+    expect(map.size()).toBe(3)
+
+    // Add ten more
+    for (let i = 1; i <= 10; i++) map.put(i, 0)
+    expect(map.size()).toBe(13)
+
+    // Remove 10
+    for (let i = 1; i <= 10; i++) map.remove(i)
+    expect(map.size()).toBe(3)
+
+    // Remove 3
+    map.remove(11)
+    map.remove(12)
+    map.remove(13)
+    expect(map.size()).toBe(0)
+  })
+
+  it('Random remove', () => {
+    const map = new HashTableSeparateChaining<number, number>()
+    for (let loop = 0; loop < LOOPS; loop++) {
+      map.clear()
+
+      // Add some random values
+      const keySet = {} as HashMap
+      for (let i = 0; i < MAX_SIZE; i++) {
+        const randomVal = randInt(-MAX_RAND_NUM, MAX_RAND_NUM)
+        keySet[randomVal] = randomVal
+        map.put(randomVal, 5)
+      }
+
+      expect(map.size()).toBe(Object.keys(keySet).length)
+
+      const keys = map.keys()
+      for (const key of keys) map.remove(key)
+      expect(map.isEmpty()).toBeTruthy()
+    }
+  })
+
+  it('Complex remove', () => {
+    const map = new HashTableSeparateChaining<{ 88: number }, number>()
+    const o1 = { 88: 1 }
+    const o2 = { 88: 2 }
+    const o3 = { 88: 3 }
+    const o4 = { 88: 4 }
+    map.add(o1, 111)
+    map.add(o2, 111)
+    map.add(o3, 111)
+    map.add(o4, 111)
+
+    map.remove(o2)
+    map.remove(o3)
+    map.remove(o1)
+    map.remove(o4)
+
+    expect(map.size()).toBe(0)
+  })
+
+  it('Random map operation', () => {
+    let jmap = {} as HashMap
+    const map = new HashTableSeparateChaining<number, number>()
+    for (let loop = 0; loop < LOOPS; loop++) {
+      map.clear()
+      jmap = {}
+      expect(map.size()).toBe(Object.keys(jmap).length)
+
+      const probability1 = Math.random()
+      const probability2 = Math.random()
+
+      const nums = genRandList(MAX_SIZE)
+      for (let i = 0; i < MAX_SIZE; i++) {
+        const r = Math.random()
+        const key = nums[i] || 0
+        const val = i
+        if (r < probability1) {
+          const value = jmap[key] === undefined ? null : jmap[key]
+          jmap[key] = val
+          expect(map.add(key, val)).toBe(value)
+        }
+
+        expect(jmap[key] === undefined ? null : jmap[key]).toBe(map.get(key))
+        expect(jmap[key] !== undefined).toBe(map.containsKey(key))
+        expect(Object.keys(jmap).length).toBe(map.size())
+
+        if (r > probability2) {
+          const jVal = jmap[key] === undefined ? null : jmap[key]
+          delete jmap[key]
+          expect(map.remove(key)).toBe(jVal)
+        }
+
+        expect(jmap[key] === undefined ? null : jmap[key]).toBe(map.get(key))
+        expect(jmap[key] !== undefined).toBe(map.containsKey(key))
+        expect(Object.keys(jmap).length).toBe(map.size())
+      }
+    }
+  })
+
+  it('Interator', () => {
+    const map = new HashTableSeparateChaining<number, number>()
+    let map2 = {} as HashMap
+    for (let loop = 0; loop < LOOPS; loop++) {
+      map.clear()
+      map2 = {} as HashMap
+      expect(map.isEmpty()).toBeTruthy()
+
+      const randNums = genRandList(MAX_SIZE)
+      for (const key of randNums) {
+        const value = map2[key] === undefined ? null : map2[key]
+        map2[key] = key
+        expect(map.add(key, key)).toBe(value)
+      }
+
+      let count = 0
+      for (const key of map) {
+        expect(map.get(key)).toBe(key)
+        expect(map.get(key)).toBe(map2[key])
+        expect(map.hasKey(key)).toBeTruthy()
+        expect(randNums.includes(key)).toBeTruthy()
+        count++
+      }
+
+      for (const key in map2) {
+        const numKey = Number.parseInt(key)
+        expect(map.get(numKey)).toBe(numKey)
+      }
+
+      // count of unique bucket index
+      const set = new Set()
+      for (const n of randNums) set.add(n)
+
+      expect(set.size).toBe(count)
+      expect(Object.keys(map2).length).toBe(count)
+    }
+  })
+})
---
+++ src/hashTableSeperateChaining.ts
@@ -0,0 +1,226 @@
+function _hashCode(key: unknown): number {
+  const s = JSON.stringify(key)
+  let h = 1
+  for (let i = 0; i < s.length; i++) {
+    h = (Math.imul(31, h) + s.charCodeAt(i)) | 0
+  }
+  return h
+}
+
+class Entry<K, V> {
+  hash: number
+
+  key: K
+
+  value: V
+
+  constructor(key: K, value: V) {
+    this.key = key
+    this.value = value
+    this.hash = _hashCode(key)
+  }
+
+  // We are not overriding the Object equals method
+  // No casting is required with this method.
+  equals(other: Entry<K, V>): boolean {
+    if (this.hash !== other.hash) return false
+    return this.key === other.key
+  }
+
+  toString(): string {
+    return this.key + ' => ' + this.value
+  }
+}
+
+export class HashTableSeparateChaining<K, V> implements Iterable<K> {
+  private static _DEFAULT_CAPACITY = 3
+  private static _DEFAULT_LOAD_FACTOR = 0.75
+
+  private _maxLoadFactor: number
+
+  private _capacity = 0
+
+  private _threshold = 0 // Integer only
+
+  private _size = 0
+  private _table: Entry<K, V>[][] = []
+
+  constructor(
+    capacity: number = HashTableSeparateChaining._DEFAULT_CAPACITY,
+    maxLoadFactor: number = HashTableSeparateChaining._DEFAULT_LOAD_FACTOR
+  ) {
+    // cannot have decimal place
+    if (capacity < 0 || capacity % 1 !== 0) throw new Error('Illegal capacity')
+    if (
+      maxLoadFactor <= 0 ||
+      Number.isNaN(maxLoadFactor) ||
+      Number.isFinite(maxLoadFactor) === false
+    ) {
+      throw new Error('Illegal maxLoadFactor')
+    }
+    this._maxLoadFactor = maxLoadFactor
+    this._capacity = Math.max(
+      HashTableSeparateChaining._DEFAULT_CAPACITY,
+      capacity
+    )
+    this._threshold = Math.round(this._capacity * maxLoadFactor)
+  }
+
+  // Returns the number of elements currently inside the hash-table
+  size(): number {
+    return this._size
+  }
+
+  // Returns true/false depending on whether the hash-table is empty
+  isEmpty(): boolean {
+    return this._size === 0
+  }
+
+  // Converts a hash value to an index. Essentially, this strips the
+  // negative sign and places the hash value in the domain [0, capacity)
+  private _normalizeIndex(keyHash: number): number {
+    return (keyHash & 0x7fffffff) % this._capacity
+  }
+
+  clear(): void {
+    this._table = []
+    this._size = 0
+  }
+
+  containsKey(key: K): boolean {
+    return this.hasKey(key)
+  }
+
+  // Returns true/false depending on whether a key is in the hash table
+  hasKey(key: K): boolean {
+    const bucketIndex = this._normalizeIndex(_hashCode(key))
+    return this._bucketSeekEntry(bucketIndex, key) !== null
+  }
+
+  // Insert, put and add all place a value in the hash-table
+  put(key: K, value: V): V | null {
+    return this.insert(key, value)
+  }
+
+  add(key: K, value: V): V | null {
+    return this.insert(key, value)
+  }
+
+  insert(key: K, value: V): V | null {
+    const newEntry = new Entry(key, value)
+    const bucketIndex = this._normalizeIndex(newEntry.hash)
+    return this._bucketInsertEntry(bucketIndex, newEntry)
+  }
+
+  // Gets a key's values from the map and returns the value.
+  // NOTE: returns null if the value is null AND also returns
+  // null if the key does not exists, so watch out..
+  get(key: K): V | null {
+    const bucketIndex = this._normalizeIndex(_hashCode(key))
+    const entry = this._bucketSeekEntry(bucketIndex, key)
+    if (entry === null) return null
+    return entry.value
+  }
+
+  // Removes a key from the map and returns the value.
+  // NOTE: returns null if the value is null AND also returns
+  // null if the key does not exists.
+  remove(key: K): V | null {
+    const bucketIndex = this._normalizeIndex(_hashCode(key))
+    return this._bucketRemoveEntry(bucketIndex, key)
+  }
+
+  // Returns the list of keys found within the hash table
+  keys(): K[] {
+    const keys: K[] = []
+    for (const bucket of this._table) {
+      if (bucket === undefined) continue
+      for (const entry of bucket) keys.push(entry.key)
+    }
+    return keys
+  }
+
+  // Returns the list of values found within the hash table
+  values(): V[] {
+    const values: V[] = []
+    for (const bucket of this._table) {
+      for (const entry of bucket) values.push(entry.value)
+    }
+    return values
+  }
+
+  [Symbol.iterator](): Iterator<K> {
+    let i = 0
+    const keys = this.keys()
+    return {
+      next: () => ({
+        // impossible to be undefined
+        value: keys[i++] as K,
+        done: i > keys.length,
+      }),
+    }
+  }
+
+  private _bucketRemoveEntry(bucketIndex: number, key: K): V | null {
+    const entry = this._bucketSeekEntry(bucketIndex, key)
+    if (entry === null) return null
+    // impossible to be undefined
+    const bucket = this._table[bucketIndex] as Entry<K, V>[]
+
+    this._table[bucketIndex] = bucket.filter((a) => a.equals(entry) === false)
+    this._size--
+    return entry.value
+  }
+
+  // Inserts an entry in a given bucket only if the entry does not already
+  // exist in the given bucket, but if it does then update the entry value
+  private _bucketInsertEntry(
+    bucketIndex: number,
+    entry: Entry<K, V>
+  ): V | null {
+    let bucket = this._table[bucketIndex]
+    if (bucket === undefined) {
+      this._table[bucketIndex] = []
+      // impossible to be undefined
+      bucket = this._table[bucketIndex] as Entry<K, V>[]
+    }
+    const existentEntry = this._bucketSeekEntry(bucketIndex, entry.key)
+
+    if (existentEntry === null) {
+      bucket.push(entry)
+      if (++this._size > this._threshold) this._resizeTable()
+      return null // Use null to indicate that there was no previous entry
+    }
+    const oldVal = existentEntry.value
+    existentEntry.value = entry.value
+    return oldVal
+  }
+
+  // Finds and returns a particular entry in a given bucket
+  // if it exists, returns null otherwise
+  private _bucketSeekEntry(bucketIndex: number, key: K): Entry<K, V> | null {
+    if (key === null) return null
+    const bucket = this._table[bucketIndex]
+    if (bucket === undefined) return null
+    for (const entry of bucket) if (entry.key === key) return entry
+    return null
+  }
+
+  // Resizes the internal table holding buckets of entries
+  private _resizeTable(): void {
+    this._capacity *= 2
+    this._threshold = Math.round(this._capacity * this._maxLoadFactor)
+    const newTable: Entry<K, V>[][] = []
+    for (let i = 0; i < this._table.length; i++) {
+      const oldBucket = this._table[i]
+      if (oldBucket === undefined) continue
+      for (const entry of oldBucket) {
+        const bucketIndex = this._normalizeIndex(entry.hash)
+        let newBucket = newTable[bucketIndex]
+        if (newBucket === undefined) newTable[bucketIndex] = newBucket = []
+        newBucket.push(entry)
+      }
+    }
+    this._table = newTable
+  }
+}
